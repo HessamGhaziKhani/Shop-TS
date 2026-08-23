04d1606541bb970dce05434e3fde1b1714c123be
Hessam <hesamghazikhani@gmail.com>
2026-08-24 03:09:32 +0330

    Fix login API endpoint

--- src/services/users/endpoints.ts
+++ src/services/users/endpoints.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 export async function login(username:string,password:string ,expiresInMins:number) {
   const {data} = await axios({
     method:"POST",
-    url:"/https://dummyjson.com/auth/login",
+    url:"https://dummyjson.com/auth/login",
     data:{
       username,
       password,
